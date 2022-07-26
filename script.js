d9f177c4f1387df5c6bc4f2b7f3367260c9762a4
James Tierney <james.tierney384@gmail.com>
2022-07-26 12:39:44 +0100

    layout changes

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// display player card
+
+
+// display computer card
+
+
+// assign 15 card per player
+
+
+// hide computer data
+
+
+//cycle through cards when attriubute is selected
+
+
+//compare selected attribute value 
+
+
+//assign player with the higher value the two card that were used 
+
+
+//when one players card amount is 0 assign the winner
+
 
